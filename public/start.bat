a104e6c27feaa1e423ab4e886a7ef3f9fb23ee84
Spender94 <166048603+Spender94@users.noreply.github.com>
2025-06-04 22:43:51 +0200

    gggg
    testtfg

---
+++ public/start.bat
@@ -0,0 +1,14 @@
+@echo off
+:: Définir le chemin vers Node.js
+set NODEJS_DIR=C:\Users\remi.jallageas\OneDrive - Wavestone\Documents\nodejs\node-v22.16.0-win-x64
+
+:: Ajouter node et npm au PATH
+set PATH=%NODEJS_DIR%;%NODEJS_DIR%\node_modules\npm\bin;%PATH%
+
+:: Installer http-server localement
+call "%NODEJS_DIR%\npm.cmd" install http-server
+
+:: Lancer le serveur
+call "%NODEJS_DIR%\npm.cmd" run start
+
+pause
